8b29bd3865be2371d1244c4dc50ce8aa7b841567
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2020-08-27 13:00:31 +0200

    added step 0 and astropy.table

---
+++ reduction/0.py
@@ -0,0 +1,81 @@
+# This step creates a table of all available _ima.fits files
+# The contents of this filelist.txt are:
+# Filename / Filter or Grism / nvisit / norbit / Time since first exposure in orbit / Time since first exposure in visit
+
+import sys
+
+sys.path.insert(0, './util')
+
+from pylab import *
+import gaussfitter, ancil, os, glob
+from astropy.io import fits
+import yaml
+from astropy.table import QTable
+from astropy.io import ascii
+
+yaml_path = "config/obs_par.yaml"
+with open(yaml_path, 'r') as file:
+    params = yaml.safe_load(file)
+
+ancil = ancil.AncillaryData(params)
+
+files = glob.glob(os.path.join(ancil.path, "*_ima.fits"))  # gets list of filenames in directory
+
+# If file exists, a previous runs has already been executed
+# Then, a file called "filelist.txt" should include all _ima.fits file names and if they are a spec or di
+prevrun = False  # os.path.exists('config/filelist.txt')
+print(prevrun)
+
+if not prevrun:  # if no previous run was done
+    filelist = open('config/filelist.txt', 'w')
+
+    expstart = np.zeros(len(files))
+    filtergrism = np.zeros(len(files), dtype=object)
+
+    # Will create a table with the properties of all _ima.fits files at the first run
+    for i in enumerate(files):
+        ima = fits.open(i[1])
+        filtergrism[i[0]] = str(ima[0].header['filter'])
+        expstart[i[0]] = ima[0].header['expstart']
+        ima.close()
+
+    torder = np.argsort(expstart)
+    files = np.array([i.split('/')[-1] for i in files])[torder]  # files are now chronologically sorted
+    times = expstart[torder]
+    print(times)
+
+    # Identify orbits and visits
+    norbits = []
+    nvisits = []
+    norbit = 0
+    nvisit = 0
+    tos = []
+    tvs = []
+    first_exp_orbit = 0
+    first_exp_visit = 0
+
+    times_diff = np.insert(np.diff(times), 0, 0)
+
+    for i in enumerate(times):
+        # if two exposures arent in the same orbit and more than an orbital period apart -> not subsequent orbits but a new visit
+        if times_diff[i[0]] * 24 * 60 > 100:
+            first_exp_visit = i[0]
+            first_exp_orbit = i[0]
+            norbit = 0
+            nvisit += 1
+        # if two exposure more than 10 min apart but less than an orbital period -> subsequent orbits
+        elif 10 < times_diff[i[0]] * 24 * 60 <= 100:
+            first_exp_orbit = i[0]
+            norbit += 1
+        # else: two exposures less than 10mins apart -> same orbit
+        norbits.append(norbit)
+        nvisits.append(nvisit)
+        to = (i[1] - times[first_exp_orbit]) * 24 * 60  # time since first exposure in orbit
+        tv = (i[1] - times[first_exp_visit]) * 24 * 60  # time since first exposure in orbit
+        tos.append(to)
+        tvs.append(tv)
+
+    t = QTable([files, filtergrism[torder], nvisits, norbits, tos, tvs],
+               names=('filenames', 'filter/grism', 'nvisit', 'norbit', 't_orbit', 't_visit'))
+
+    ascii.write(t, 'config/filelist.txt', format='ecsv')
--- reduction/zero_extract.py
+++ reduction/zero_extract.py
@@ -6,6 +6,7 @@ from pylab import *
 import gaussfitter, ancil, os, glob
 from astropy.io import fits
 import yaml
+from astropy.io import ascii
 
 yaml_path = "config/obs_par.yaml"
 with open(yaml_path, 'r') as file:
@@ -13,95 +14,22 @@ with open(yaml_path, 'r') as file:
 
 ancil = ancil.AncillaryData(params)
 
-files = glob.glob(os.path.join(ancil.path, "*_ima.fits"))				#gets list of filenames in directory
-
-
-#print "tstart.txt needs to be modified."
-#print "It should contain a time before the start time of each VISIT (not orbit), and have 99999. appended to it"
-
 if ancil.output ==True:
 	f = open('config/xrefyref.txt', 'w')						#opens file to store positions of reference pixels
-	tstart = open('config/tstart.txt', 'w')					#opens file to store exp start times, rounded down
 
-# If file exists, a previous runs has already been executed
-# Then, a file called "filelist.txt" should include all _ima.fits file names and if they are a spec or di
-prevrun = os.path.exists('config/filelist.txt')
+filetable = ascii.read('config/filelist.txt')
 
-if not prevrun:
-	filelist = open('config/filelist.txt', 'w')
+mask_di = filetable['filter/grism'] == ancil.FILTER
 
-	expstart = np.zeros(len(files))
-	filtergrism = np.zeros(len(files), dtype=object)
+files_di = [ancil.path + '/' + i for i in filetable['filenames'][mask_di].data]
+print(files_di)
 
-	# Will create a table with the properties of all _ima.fits files at the first run
-	for i in enumerate(files):
-		filename = i[1]
-		ima = fits.open(i[1])
-		filtergrism[i[0]] = str(ima[0].header['filter'])
-		expstart[i[0]] = ima[0].header['expstart']
-
-	print(expstart)
-	print(filtergrism)
-
-	torder = np.argsort(expstart)
-	files_sorted = np.array([i.split('/')[-1] for i in files])[torder]
-	times = expstart[torder]
-	#np.savetxt(filelist, list(zip(files_sorted, filtergrism[torder], expstart[torder])), fmt="%s")
-
-	# Identify orbits and visits
-
-	norbits = []
-	nvisits = []
-	norbit = 0
-	nvisit = 0
-	torbs = []
-	torb = 0
-	norbits.append(norbit)
-	nvisits.append(nvisit)
-	torbs.append(torb)
-	current_first = 0
-
-	for i in enumerate(times[1:]):
-		torb = (times[i[0]+1]-times[current_first])*24*60 # time since first exposure in orbit
-		print(torb)
-		# if two exposures less than 10mins apart -> same orbit
-		if np.diff(times)[i[0]]*24*60 < 10:
-			torbs.append(torb)
-			norbits.append(norbit)
-			nvisits.append(nvisit)
-		# if two exposures arent in the same orbit and more than an orbital period apart -> not subsequent orbits but a new visit
-		elif np.diff(times)[i[0]]*24*60 > 100:
-			torb = 0
-			torbs.append(torb)
-			current_first = i[0]+1
-			norbit = 0
-			nvisit += 1
-			norbits.append(norbit)
-			nvisits.append(nvisit)
-		# if two exposure more than 10 min apart but less than an orbital period -> subsequent orbits
-		else:
-			torb = 0
-			torbs.append(torb)
-			current_first = i[0]+1
-			norbit += 1
-			norbits.append(norbit)
-			nvisits.append(nvisit)
-
-	np.savetxt(filelist, list(zip(files_sorted, filtergrism[torder], nvisits, norbits, torbs, (times-times[0])*24*60)), fmt="%s")
-	files = [ancil.path + '/' + i for i in files_sorted[filtergrism[torder] == ancil.FILTER]]
-else:
-	files, fg,_,_,_,_ = np.loadtxt('config/filelist.txt', dtype=str).T
-	files = [ancil.path + '/' + i for i in files[fg == ancil.FILTER]]
-
-
-
-t_last = 0.
-for i in enumerate(files):
-	ima = fits.open(files[i[0]])
+for i in enumerate(files_di):
+	ima = fits.open(i[1])
 	print(ima[0].header['OBSTYPE'])
 
-	if(ima[0].header['filter'] == ancil.FILTER):
-		print("filename", files[i[0]])
+	if (ima[0].header['filter'] == ancil.FILTER):
+		print("filename", [i[1]])
 		LTV1 = ima[1].header['LTV1']					#X offset to get into physical pixels
 		LTV2 = ima[1].header['LTV2']					#Y offset to get to physical pixels
 		nrow = len(ima[1].data[:,0])
@@ -127,14 +55,9 @@ for i in enumerate(files):
 		
 		if ancil.output==True:
 			print(t, results[3]+ancil.rmin-LTV1, results[2]+ancil.cmin-LTV2, file=f)
-			if t - t_last > 10./24.:							#checks if this image corresponds to a new visit
-				print(np.floor(t), file=tstart)	
-				t_last = t
-		
-		print(t, results[3]+ancil.rmin-LTV1, results[2]+ancil.cmin-LTV2, files[i[0]]) 		#fit results
+
+		print(t, results[3]+ancil.rmin-LTV1, results[2]+ancil.cmin-LTV2, [i[1]]) 		#fit results
 	ima.close()
 
 if ancil.output==True:
 	f.close()
-	print(99999., file=tstart)
-	tstart.close()
