ec5e15e01a2449da4dc02484fce90434c5f375df
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2021-04-21 23:50:44 +0200

    saves optionally more intermediate steps

--- reduction/util/ancil.py
+++ reduction/util/ancil.py
@@ -11,6 +11,11 @@ class AncillaryData:
 		self.direct_image_output = obs_par['direct_image_output']#flag specifying whether coordinates are output to a file
 		self.diagnostics = obs_par['direct_image_diagnostics']#makes diagnostic plot if true
 
+		self.trace_image_output = obs_par['trace_image_output']
+		self.all_image_output = obs_par['all_image_output']
+		self.trace_image_show = obs_par['trace_image_show']
+		self.all_image_show = obs_par['all_image_show']
+
 		# selects the boundaries of the region where the 2d Gaussian is fit,
 		# these were selected by eye
 		self.rmin = obs_par['di_rmin']
@@ -20,15 +25,46 @@ class AncillaryData:
 		self.filter = obs_par['FILTER']
 
 		# new ones for 2.py
+
 		#if os.path.isfile("config/filelist.txt"):
 		if fit_par != None:
-			self.flat = obs_par['flat']
+
+			self.grism = obs_par['GRISM']
+
+			filetable = ascii.read('config/filelist.txt')
+			mask = filetable['filter/grism'] == self.grism
+			self.files = [self.path + '/' + i for i in filetable['filenames'][mask].data]
+			self.times = filetable['times'][mask].data
+			self.scans = filetable['scan'][mask].data
+			self.orbnum = filetable['norbit'][mask].data
+			self.visnum = filetable['nvisit'][mask].data
+
+
+			refpix = np.genfromtxt("config/xrefyref.txt")  # reads in reference pixels for each visit and sorts them by time
+			idx = np.argsort(refpix[:, 0]) #sort by time
+			self.refpix = refpix[idx]  # reference pixels from direct image
+
+
+			self.platescale = 0.13 #IR detector has plate scale of 0.13 arcsec/pixel
+
+			f = fits.open(self.files[0])
+			self.POSTARG1 = f[0].header['POSTARG1'] #x-coordinate of the observer requested target offset
+			self.POSTARG2 = f[0].header['POSTARG2'] #y-coordinate of the observer requested target offset
+			self.LTV1 = int(f[1].header['LTV1'])
+			self.LTV2 = int(f[1].header['LTV2'])
 
 			self.BEAMA_i = obs_par['BEAMA_i']  # start of first order trace
 			self.BEAMA_f = obs_par['BEAMA_f']  # end of first order trace
+
+			self.subarray_size = f[1].header['SIZAXIS1']  # size of subarray
+
+
+			self.flat = obs_par['flat']
+
+
 			self.npix = self.BEAMA_f - self.BEAMA_i  # length of trace
 
-			self.grism = obs_par['GRISM']
+
 
 			self.plot_trace = obs_par['plot_trace']
 			self.diagnostics = obs_par['diagnostics']
@@ -41,7 +77,6 @@ class AncillaryData:
 			self.nvisit = obs_par['nvisit']
 			self.norb = obs_par['norb']
 
-			self.platescale = 0.13
 
 			self.skyrmin = obs_par['skyrmin']
 			self.skyrmax = obs_par['skyrmax']
@@ -49,21 +84,10 @@ class AncillaryData:
 			self.skycmax = obs_par['skycmax']
 
 
-			filetable = ascii.read('config/filelist.txt')
-			mask = filetable['filter/grism'] == self.grism
-			self.files = [self.path + '/' + i for i in filetable['filenames'][mask].data]
-			self.orbnum = filetable['norbit'][mask].data
-			self.visnum = filetable['nvisit'][mask].data
 
-			f = fits.open(self.files[0])
 
-			self.LTV1 = int(f[1].header['LTV1'])
-			self.LTV2 = int(f[1].header['LTV2'])
 
-			self.POSTARG1 = f[0].header['POSTARG1']
-			self.POSTARG2 = f[0].header['POSTARG2']
 
-			self.subarray_size = f[1].header['SIZAXIS1']  # size of subarray
 
 			self.ra = f[0].header['ra_targ'] * math.pi / 180.0  # stores right ascension
 			self.dec = f[0].header['dec_targ'] * math.pi / 180.0  # stores declination
@@ -73,22 +97,21 @@ class AncillaryData:
 
 			self.wavegrid = None
 
-			#tstart = np.genfromtxt("config/tstart.txt")  # approximate start times for each observation (strictly before the actual start)
-			refpix = np.genfromtxt("config/xrefyref.txt")  # reads in reference pixels for each visit and sorts them by time
+
+
+
 
 			self.one_di_per_visit = obs_par['one_di_per_visit']
 			#if self.one_di_per_visit == True: norb = nvisit
 
-			idx = np.argsort(refpix[:, 0])
-			self.refpix = refpix[idx]  # reference pixels from direct image
 
 			self.torbstart = self.refpix[:, 0]  # start times for each orbit
 			self.torbstart = np.append(self.torbstart, 1.0e10)  # appends large value to make get_orbnum routine work
 
 			#self.tstart = tstart
 
-			self.coordtable = []  # table of spacecraft coordinates
-			for i in range(self.nvisit): self.coordtable.append("bjd_conversion/horizons_results_v" + str(i) + ".txt")
+			self.coordtable = ["bjd_conversion/horizons_results.txt"]  # table of spacecraft coordinates
+			#for i in range(self.nvisit): self.coordtable.append("bjd_conversion/horizons_results_v" + str(i) + ".txt")
 
 
 			def make_dict(table):
