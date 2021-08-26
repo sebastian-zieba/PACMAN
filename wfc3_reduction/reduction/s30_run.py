bdceda84f4fce3009f3d32634fd2f56583770f90
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2021-08-26 18:08:28 +0200

    save lsq result in file

--- wfc3_reduction/reduction/s30_run.py
+++ wfc3_reduction/reduction/s30_run.py
@@ -188,6 +188,16 @@ def run30(eventlabel, workdir, meta=None):
     plt.subplots_adjust(hspace=0.01)
     plt.savefig(meta.workdir + meta.fitdir + '/ls_{0}.png'.format(datetime), dpi=700, tight_layout=True)
 
+
+    f_lsq =  open(meta.workdir + meta.fitdir + "/lsq_res_{0}.txt".format(meta.fittime), 'w')
+    files_name_ending = [float(files[ii].split('speclc')[-1].split('.txt')[0]) for ii in range(len(files))]
+    rprs_vals_lsq = [vals[ii][idxs[0][5]] for ii in range(len(vals))]
+    rprs_errs_lsq = [errs[ii][idxs[0][5]] for ii in range(len(errs))]
+    rprs_idxs_lsq = [idxs[ii][idxs[0][5]] for ii in range(len(idxs))]
+    for row in zip(files_name_ending, rprs_vals_lsq, rprs_errs_lsq, rprs_idxs_lsq):
+        print("{: <10} {: <25} {: <25} {: <25}".format(*row), file=f_lsq)
+    f_lsq.close()
+
     return meta
 
 def update_clips(clips_array):
