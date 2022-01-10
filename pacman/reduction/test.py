d8baa766a77cd69210ec0015012cc4e7f46954f4
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2022-01-11 00:57:40 +0100

    cleaned up

--- pacman/reduction/test.py
+++
@@ -1,5 +0,0 @@
-import os
-print('/'.join(os.path.realpath(__file__).split('/')))
-print('/'.join(os.path.realpath(__file__).split('/')[:-1]))
-print('/'.join(os.path.realpath(__file__).split('/')[:-2]))
-print('/'.join(os.path.realpath(__file__).split('/')[:-3]))
