952a5ee6435a916e6de826797967f02c8c604fdb
Ivelina Momcheva <ivelina.momcheva@gmail.com>
2022-04-01 15:34:07 +0200

    Moving directories

--- pacman/lib/models/divide_white.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0,'..')
-import numpy as np
-
-def divide_white(t, data, params):
-    return data.white_systematics 
--- pacman/lib/models/upstream_downstream.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0,'..')
-import numpy as np
-
-def upstream_downstream(t, data, params, visit = 0):
-    scale = params
-    scale = scale[0][visit]
-
-    return 1. + scale*data.scan_direction[data.vis_idx[visit]]
