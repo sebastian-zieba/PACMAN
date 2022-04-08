b67ff936ce21c628bde2df3de64c2f433cceaa00
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2022-04-08 02:16:42 +0200

    fixed docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,7 @@ release = '0.0.1'
 # ones.
 extensions = ['sphinx_rtd_theme', 'sphinx.ext.todo', 'sphinx.ext.viewcode',
               'sphinx.ext.autodoc', 'numpydoc', 'nbsphinx', 'myst_parser',
-              'sphinx.ext.autosectionlabel', 'sphinx.ext.napoleon', 'RunNotebook.notebook_sphinxext',
-              'RunNotebook.notebookcell_sphinxext']
+              'sphinx.ext.autosectionlabel', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
