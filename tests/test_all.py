0c355b33b7522b9377301cb3e5943e4e380a4194
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2022-04-12 16:35:35 +0200

    fixing paths

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,7 +5,7 @@ from astropy.io import ascii
 from astroquery.mast import Observations
 from astropy.io import fits
 
-#sys.path.insert(0, '/home/zieba/Desktop/Projects/Open_source/PACMAN/')
+#sys.path.insert(0, '/home/zieba/Desktop/Projects/Open_source/PACMAN/src/')
 #print(sys.path)
 
 from pacman.lib import util
