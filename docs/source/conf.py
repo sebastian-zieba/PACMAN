c88a3a22cae81b73126f96720d8a3da14581557a
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2022-04-12 12:33:23 +0200

    fixing paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,10 +14,10 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'pacman')))
-sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../../pacman'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../../src/pacman'))
 
 # -- Project information -----------------------------------------------------
 
