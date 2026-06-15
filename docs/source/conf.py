1cc04681c053ff4774a88fd263bb7d3b09dac75a
Sebastian Zieba <sebastian.zieba@cfa.harvard.edu>
2026-06-15 16:44:08 -0400

    updating docs for v0.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,11 +23,11 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 # -- Project information -----------------------------------------------------
 
 project = 'PACMAN'
-copyright = '2024, Sebastian Zieba, Laura Kreidberg'
+copyright = '2026, Sebastian Zieba, Laura Kreidberg'
 author = 'Sebastian Zieba, Laura Kreidberg'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
